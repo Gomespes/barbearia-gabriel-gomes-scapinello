8ba1d414cf137eded9ee77199b068dc193243483
Gomespes <gabriel.scapinello@gmail.com>
2025-11-25 20:28:12 -0300

    Delete barbearia directory

--- barbearia/back-php/listar-depoimento.php
+++
@@ -1,9 +0,0 @@
-<?php
- $con = new PDO("mysql:host=localhost;dbname=barbearia;charset=utf8", "root", "");
-//con recebe uma nova instancia PDO(conexao entre php e o banco) //usuario, senha
-
-$sql = "select nomeCliente, nota, descricao from depoimentos";
-$qry = $con->prepare($sql);
-$qry->execute();
-
-echo json_encode($qry->fetchAll(PDO::FETCH_ASSOC));
